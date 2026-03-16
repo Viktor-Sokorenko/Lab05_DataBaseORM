9d0be5bf56262624392b4bde456dc14dd5f0d5af
Viktor-Sokorenko <viktor.sokorenko@student.howest.be>
2026-03-16 11:07:31 +0100

    Implement Species resource with SQLModel model, repository and API router

---
+++ lab05_birds/lab05_birds/repositories/species.py
@@ -0,0 +1,20 @@
+from sqlmodel import Session, select
+from models.species import Species, SpeciesCreate
+
+class SpeciesRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_all(self):
+        statement = select(Species)
+        return self.session.exec(statement).all()
+
+    def get_one(self, species_id: int):
+        return self.session.get(Species, species_id)
+
+    def insert(self, payload: SpeciesCreate):
+        item = Species.model_validate(payload)
+        self.session.add(item)
+        self.session.commit()
+        self.session.refresh(item)
+        return item
